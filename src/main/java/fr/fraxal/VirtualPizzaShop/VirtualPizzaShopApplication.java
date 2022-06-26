4f09cafc6d313009a1594735eb5472ded451b92c
CreepDeluxe <creepdeluxetv@gmail.com>
2022-06-26 14:31:22 +0200

    Init from Eclipse

---
+++ src/main/java/fr/fraxal/VirtualPizzaShop/VirtualPizzaShopApplication.java
@@ -0,0 +1,13 @@
+package fr.fraxal.VirtualPizzaShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPizzaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualPizzaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/fraxal/VirtualPizzaShop/VirtualPizzaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fraxal.VirtualPizzaShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPizzaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
